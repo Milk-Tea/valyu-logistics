7d6e41048dfb874971bc224a40cf35b75967c2d3
Staphnie Hung <staphnie-hung@hotmail.com>
2025-05-05 12:10:20 +0200

    Clean project state with proper .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { storyblokInit, apiPlugin } from "@storyblok/react";
 
-// Optional: Storyblok live preview
 export default defineConfig({
   plugins: [react()],
-  base: '/valyu-logistics/'
+  base: '/valyu-logistics/',
+  build: {
+    rollupOptions: {
+      external: ['@storyblok/react'],
+    },
+  },
 })
