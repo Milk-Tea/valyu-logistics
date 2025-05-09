a15ee530068e28a7c16675ab2bbfbcd55a68ae66
Staphnie Hung <staphnie-hung@hotmail.com>
2025-05-09 17:22:09 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/valyu-logistics/',
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-});
+  base: mode === 'production' ? '/valyu-logistics/' : '/',
+}));
